228cfbf06abf0c216a076c44032e7b6d8b6df7fd
Era Dorta <e.dorta@tudelft.nl>
2024-10-09 14:22:16 +0200

    Add example of entrypoint

--- project_template/src/{{python_name}}/main.py
+++ project_template/src/{{python_name}}/main.py
@@ -21,6 +21,10 @@ def add(a: int, b: int) -> int:
     return a + b
 
 
-if __name__ == "__main__":
+def main():
     print(f"Running {{ python_name }} version {__version__}")
-    add(1, 2)
+    print(f"Result is: 1+2 = {add(1, 2)}")
+
+
+if __name__ == "__main__":
+    main()
