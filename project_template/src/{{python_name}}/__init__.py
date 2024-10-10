aec1864051963eaa95ef3554f1e89c57a6aa5a85
Era Dorta <e.dorta@tudelft.nl>
2024-10-10 15:15:38 +0200

    Fix bug with version not being updated on dev environments

--- project_template/src/{{python_name}}/__init__.py
+++ project_template/src/{{python_name}}/__init__.py
@@ -4,7 +4,20 @@
 
 from __future__ import annotations
 
-from importlib.metadata import version
+try:
+    # Development way of getting the version, always correct, it is dynamically updated
+
+    # setuptools_scm might not included in the runtime dependencies
+    from setuptools_scm import get_version
+
+    # This will fail with LookupError if the package is not installed in editable mode
+    # or if Git is not installed
+    __version__ = get_version(root="../..", relative_to=__file__)
+except (ImportError, LookupError):
+    # Standard way of getting the version, pulls it from the _version.py file
+    from importlib.metadata import version
+
+    __version__ = version(__name__)
 
 __all__ = ("__version__",)
-__version__ = version(__name__)
+
