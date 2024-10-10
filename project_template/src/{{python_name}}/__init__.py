d40947609de976f01908c4504136e74cefcf73a2
Era Dorta <e.dorta@tudelft.nl>
2024-10-10 16:36:07 +0200

    Fix import error on shallow cloned repos

--- project_template/src/{{python_name}}/__init__.py
+++ project_template/src/{{python_name}}/__init__.py
@@ -7,13 +7,14 @@ from __future__ import annotations
 try:
     # Development way of getting the version, always correct, it is dynamically updated
 
-    # setuptools_scm might not included in the runtime dependencies
+    # setuptools_scm might not included in the runtime dependencies, raising ImportError
+    # it also fails on the gitlab CI with a UserWarning error
     from setuptools_scm import get_version
 
     # This will fail with LookupError if the package is not installed in editable mode
     # or if Git is not installed
     __version__ = get_version(root="../..", relative_to=__file__)
-except (ImportError, LookupError):
+except (ImportError, LookupError, UserWarning):
     # Standard way of getting the version, pulls it from the _version.py file
     from importlib.metadata import version
 
