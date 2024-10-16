91dc154a52589c1995f0d8b2ea27fa2d58728a6a
Era Dorta <e.dorta@tudelft.nl>
2024-10-16 13:42:43 +0200

    More robust way to get the package version

--- project_template/src/{{python_name}}/__init__.py
+++ project_template/src/{{python_name}}/__init__.py
@@ -4,20 +4,31 @@
 
 from __future__ import annotations
 
-try:
-    # Development way of getting the version, always correct, it is dynamically updated
+import json
+from importlib.metadata import Distribution, version
 
-    # setuptools_scm might not included in the runtime dependencies, raising ImportError
-    # it also fails on the gitlab CI with a UserWarning error
-    from setuptools_scm import get_version
 
-    # This will fail with LookupError if the package is not installed in editable mode
-    # or if Git is not installed
-    __version__ = get_version(root="../..", relative_to=__file__)
-except (ImportError, LookupError, UserWarning):
-    # Standard way of getting the version, pulls it from the _version.py file
-    from importlib.metadata import version
+def _is_editable():
+    direct_url = Distribution.from_name("ati_template_test_2").read_text(
+        "direct_url.json"
+    )
+    return json.loads(direct_url).get("dir_info", {}).get("editable", False)
 
+
+if _is_editable():
+    # Development way of getting the version, it is dynamically updated on each call
+    try:
+        # setuptools_scm is not included in the runtime virtual environment
+        # importing it raises an ImportError,
+        # it also fails in the gitlab CI with a UserWarning error
+        from setuptools_scm import get_version
+
+        # This will fail with LookupError if Git is not installed
+        __version__ = get_version(root="../..", relative_to=__file__)
+    except (ImportError, LookupError, UserWarning):
+        __version__ = version(__name__)
+else:
+    # Get the version as specified by the wheel
     __version__ = version(__name__)
 
 __all__ = ("__version__",)
